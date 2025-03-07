067c7b588cbb5c340d4b5f108fc8de50cae60b21
notthat29 <jcreyes24@up.edu.ph>
2025-03-07 11:08:43 +0800

    Add files

---
+++ MICROBLAZE_V_AXI.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_1_1/design_1_clk_wiz_1_1_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2024.1.2 (win64) Build 5164865 Thu Sep  5 14:37:11 MDT 2024
+// Date        : Fri Mar  7 10:53:43 2025
+// Host        : DESKTOP-LQBVH1M running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/JEFF/MICROBLAZE_V_AXI/MICROBLAZE_V_AXI.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_1_1/design_1_clk_wiz_1_1_stub.v
+// Design      : design_1_clk_wiz_1_1
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tsbg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module design_1_clk_wiz_1_1(clk_out1, resetn, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="resetn,locked,clk_in1" */
+/* synthesis syn_force_seq_prim="clk_out1" */;
+  output clk_out1 /* synthesis syn_isclock = 1 */;
+  input resetn;
+  output locked;
+  input clk_in1;
+endmodule
---
+++ MICROBLAZE_V_AXI.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_1_1/design_1_clk_wiz_1_1_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.1.2 (win64) Build 5164865 Thu Sep  5 14:37:11 MDT 2024
+-- Date        : Fri Mar  7 10:53:43 2025
+-- Host        : DESKTOP-LQBVH1M running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/JEFF/MICROBLAZE_V_AXI/MICROBLAZE_V_AXI.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_1_1/design_1_clk_wiz_1_1_stub.vhdl
+-- Design      : design_1_clk_wiz_1_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a200tsbg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_clk_wiz_1_1 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end design_1_clk_wiz_1_1;
+
+architecture stub of design_1_clk_wiz_1_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,resetn,locked,clk_in1";
+begin
+end;
